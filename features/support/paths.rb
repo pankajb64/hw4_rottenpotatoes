c22fbc5507e41a6c423f9fb6f221d0bccdf980b8
Pankaj Bhambhani <pankajb64@gmail.com>
2012-06-28 19:45:17 +0530

    Happy Path Cucumber and RSpec done

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -19,6 +19,11 @@ module NavigationHelpers
     when /^the edit page for "(.*)"$/
       edit_movie_path(Movie.find_by_title($1))
 
+    when /^the details page for "(.*)"$/
+      movie_path(Movie.find_by_title($1))
+    
+    when /^the Similar Movies page for "(.*)"$/
+      similar_director_movie_path(Movie.find_by_title($1))  
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
